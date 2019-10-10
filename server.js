1d34c96834066f493f811673b11f1c3c0c51516f
AJ Arriola <angelojames.arriola@gmail.com>
2019-10-10 01:50:54 -0700

    Add port for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
